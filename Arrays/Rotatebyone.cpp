c9b224b46bf7ceea2f9c87b847857537140537f6
Angshu101 <angshu101.9733@gmail.com>
2022-05-26 18:14:15 +0530

    commit

---
+++ Arrays/Rotatebyone.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int* rotate_array(int arr[],int size){
+int temp;
+temp=arr[size-1];
+for(int i=(size-2);i>=0;i--){
+    arr[i+1]=arr[i];
+}
+arr[0]=temp;
+
+return arr;
+}
+int main(){
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+int N;
+cout<<"Enter the siz of the array";
+cin>>N;
+int arr[N];
+for(int i=0;i<N;i++){
+    cin>>arr[i];
+}
+int* p;
+p=rotate_array(arr,N);
+cout<<"The rotated array is :- "<<endl;
+for(int i=0;i<N;i++){
+    cout<<p[i]<<" ";
+}
+delete[] p;
+return 0;
+}
